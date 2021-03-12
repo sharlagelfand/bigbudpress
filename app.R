1655cfdb6b4b96750613eda4bcf6985d3507dff4
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-03-12 14:19:34 -0500

    Fixes for uploading to shinyapps, cuter front page

--- app.R
+++ app.R
@@ -1,17 +1,20 @@
 library(shiny)
 library(ggplot2)
-library(rnaturalearth)
 library(dplyr)
 library(shinycssloaders)
 library(reactable)
 library(showtext)
+library(curl) # Needed for shinyapps.io? IDK why
+library(sf)
 
 font_add_google("Corben")
 showtext_auto()
 
 last_week <- readRDS("data/last_week.rds")
 last_year <- readRDS("data/last_year.rds")
-world <- ne_countries(scale = "medium", returnclass = "sf")
+world <- readRDS("data/world.rds")
+
+st_crs(world) <- 4326 # Re-set CRS because Shinyapps uses some other version of GDAL (??) or something idk
 
 logo <- a(href = "/", img(src = "logo.webp", alt = "BIG BUD PRESS", height = "100px"))
 footer <- div(class = "bb-footer", p("Made with love and #rstats by", a("Sharla Gelfand", href = "https://sharla.party", target = "_blank")))
@@ -28,20 +31,24 @@ ui <-
       footer = footer,
       tabPanel(
         "Hello!",
+        div(
+          style = "position: fixed; left: 0; top: 0; z-index: -1; background-size:cover;",
+          img(src = "banner.png")
+        ),
         column(
           width = 12,
-          h3("Hello!"),
-          span(style = "font-size: 20px", "This is an exploration of branding a web app, inspired by (ripping off?) the wonderful", a("Big Bud Press", href = "https://bigbudpress.com", target = "_blank"), ", a Los Angeles based clothing brand specializing in unisex, everyday, and ethical goods. It's just for fun! You can read more about the making of in", a("this blog post", href = "", target = "_blank"), "and see", a("the code here.", href = "", target = "_blank")),
-          br(),
-          br(),
-          img(src = "about-bb.webp", style = "display: block; margin-left: auto; margin-right: auto;")
+          wellPanel(
+            style = "border: none; background-color: rgba(255, 255, 255, 0.5); box-shadow: none; --webkit-box-shadow: none;",
+            h1("Hello!"),
+            p("This is an exploration of branding a web app, inspired by (ripping off?) the wonderful", a("Big Bud Press", href = "https://bigbudpress.com", target = "_blank"), ", a Los Angeles based clothing brand specializing in unisex, everyday, and ethical goods. It's just for fun! You can read more about the making of in", a("this blog post", href = "", target = "_blank"), "and see", a("the code here.", href = "", target = "_blank"))
+          )
         )
       ),
       tabPanel(
         "Google Trends",
         column(
           width = 9,
-          p("This page captures popularity of the search term", span("Big Bud Press"), "from Google Trends. Numbers are all relative to the highest data point for a given region or time - 100 is peak popularity, 50 means half as popular. Data was obtained via the R package", a("gtrendsR", href = "https://cran.r-project.org/web/packages/gtrendsR/index.html", target = "_blank"), "on March 11, 2021, so only goes up til then!")
+          p("This page captures popularity of the search term \"Big Bud Press\" from Google Trends. Numbers are all relative to the highest data point for a given region or time - 100 is peak popularity, 50 means half as popular. Data was obtained via the R package", a("gtrendsR", href = "https://cran.r-project.org/web/packages/gtrendsR/index.html", target = "_blank"), "on March 11, 2021, so only goes up til then!")
         ),
         column(
           width = 3,
@@ -95,7 +102,7 @@ server <- function(input, output, session) {
     ggplot(time_data()[["interest_over_time"]], aes(x = date, y = hits)) +
       geom_line(colour = "#C585E6", size = 2) +
       labs(x = NULL, y = NULL) +
-      theme_minimal(base_family = "Corben") +
+      theme_minimal(base_size = 18, base_family = "Corben") +
       theme(axis.text = element_text(face = "bold"))
   })
 
@@ -108,7 +115,7 @@ server <- function(input, output, session) {
       geom_sf(aes(fill = hits), lwd = 0.25) +
       scale_fill_gradient(na.value = "white", low = "#C585E6", high = "#4D0268", limits = c(0, 100), breaks = c(0, 50, 100), name = NULL) +
       guides(fill = guide_colourbar(ticks = FALSE)) +
-      theme_minimal(base_family = "Corben") +
+      theme_minimal(base_size = 18, base_family = "Corben") +
       theme(
         legend.position = "bottom",
         legend.text = element_text(face = "bold")
--- data.R
+++ data.R
@@ -1,7 +1,10 @@
 library(gtrendsR)
+library(rnaturalearth)
 
 last_year <- gtrends("big bud press", time = "today 12-m", low_search_volume = TRUE)
 last_week <- gtrends("big bud press", time = "now 7-d", low_search_volume = TRUE)
+world <- ne_countries(scale = "medium", returnclass = "sf")
 
-saveRDS(last_year, "last_year.rds")
-saveRDS(last_week, "last_week.rds")
+saveRDS(last_year, "data/last_year.rds")
+saveRDS(last_week, "data/last_week.rds")
+saveRDS(world, "data/world.rds")
